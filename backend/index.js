27c90f1615dabe18e7ed87f7807310693ffa7eb4
omrane13 <143047533+omrane13@users.noreply.github.com>
2025-07-18 14:56:41 +0100

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser');
+const cors =require('cors');
+const AuthRouter = require('./Routes/AuthRouter');
+const ProductRouter = require('./Routes/ProductRouter');
+
+
+
+require('dotenv').config();
+require('./Models/db');
+const PORT= process.env.PORT || 8000;
+
+
+app.get('/ping', (req,res) => {
+    res.send('pong');});
+app.use(bodyParser.json());
+//app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+})
